c31c86cf39724ecebe816cdd23ffac250f5f70f4
dblate <173983476@qq.com>
2018-05-04 21:02:17 +0800

    增加几种事件类型

--- src/js/class-name-manager.js
+++ src/js/class-name-manager.js
@@ -4,17 +4,24 @@
  * @date 2018/5/4
  */
 
-import larkplayer from 'larkplayer';
+import {Plugin, DOM} from 'larkplayer';
 
 import ClassNames from './class-names';
 import toTitleCase from './utils/to-title-case';
-
-const Plugin = larkplayer.Plugin;
+import featureDetector from './utils/feature-detector';
 
 export default class ClassNameManager extends Plugin {
     constructor(player, options) {
         super(player, options);
 
+        this.handleMouseEnter = this.handleMouseEnter.bind(this);
+        this.handleMouseMove = this.handleMouseMove.bind(this);
+        this.handleMouseLeave = this.handleMouseLeave.bind(this);
+        this.handleTouchStart = this.handleTouchStart.bind(this);
+        this.handleTouchEnd = this.handleTouchEnd.bind(this);
+
+        this.activeTimeoutHandler = null;
+        this.activeTimeout = 3000;
         this.events = [
             'loadstart',
             'canplay',
@@ -35,16 +42,85 @@ export default class ClassNameManager extends Plugin {
             this.player.on(event, this[callbackName]);
         });
 
-        this.activeTimeoutHandler = null;
-        this.activeTimeout = 3000;
+        if (featureDetector.touch) {
+            this.player.on('touchstart', this.handleTouchStart);
+            this.player.on('touchend', this.handleTouchEnd);
+        } else {
+            this.player.on('mouseenter', this.handleMouseEnter);
+            this.player.on('mousemove', this.handleMouseMove);
+            this.player.on('mouseleave', this.handleMouseLeave);
+        }
     }
 
     dispose() {
+        if (featureDetector.touch) {
+            this.player.off('touchstart', this.handleTouchStart);
+            this.player.off('touchend', this.handleTouchEnd);
+        } else {
+            this.player.off('mouseenter', this.handleMouseEnter);
+            this.player.off('mousemove', this.handleMouseMove);
+            this.player.off('mouseleave', this.handleMouseLeave);
+        }
         this.events.forEach(event => {
             this.player.off(event, this[`handle${toTitleCase(event)}`]);
         });
 
         super.dispose();
+    }
+
+    handleTouchStart(event) {
+        // 当控制条显示并且手指放在控制条上时
+        if (this.player.hasClass(ClassNames.ACTIVE)) {
+            if (DOM.parent(event.target, 'lark-play-button')
+                || DOM.parent(event.target, 'lark-control-bar')) {
+
+                clearTimeout(this.activeTimeoutHandler);
+            }
+        }
+    }
+
+    handleTouchEnd(event) {
+        clearTimeout(this.activeTimeoutHandler);
+
+        // 点在播放按钮或者控制条上，（继续）展现控制条
+        let clickOnControls = false;
+        // @todo 处理得不够优雅
+        if (DOM.parent(event.target, 'lark-play-button')
+            || DOM.parent(event.target, 'lark-control-bar')) {
+
+            clickOnControls = true;
+        }
+
+        if (!clickOnControls) {
+            this.player.toggleClass(ClassNames.ACTIVE);
+        }
+
+        if (this.player.hasClass(ClassNames.ACTIVE)) {
+            this.activeTimeoutHandler = setTimeout(() => {
+                this.player.removeClass(ClassNames.ACTIVE);
+            }, this.activeTimeout);
+        }
+    }
+
+    handleMouseEnter(event) {
+        clearTimeout(this.activeTimeoutHandler);
+
+        if (!this.player.hasClass(ClassNames.ACTIVE)) {
+            this.player.addClass(ClassNames.ACTIVE);
+        }
+
+        this.activeTimeoutHandler = setTimeout(() => {
+            this.player.removeClass(ClassNames.ACTIVE);
+        }, this.activeTimeout);
+    }
+
+    handleMouseMove(event) {
+        this.handleMouseEnter(event);
+    }
+
+    handleMouseLeave(event) {
+        clearTimeout(this.activeTimeoutHandler);
+        this.player.removeClass(ClassNames.ACTIVE);
     }
 
     /**
@@ -161,7 +237,7 @@ export default class ClassNameManager extends Plugin {
      */
     handleFirstplay() {
         // @todo 不清楚有什么用
-        this.player.addClass('lark-has-started');
+        this.player.addClass(ClassNames.HAS_START);
 
         clearTimeout(this.activeTimeoutHandler);
         this.player.addClass(ClassNames.ACTIVE);
